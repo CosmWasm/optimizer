16bbaca468cbca03795716f551e526acec562d8f
Simon Warta <simon@warta.it>
2020-12-07 15:29:24 +0100

    Fix loop in script

--- optimize.sh
+++ optimize.sh
@@ -21,22 +21,22 @@ contractdirs="$@"
 # This parameter allows us to mount a folder into docker container's "/code"
 # and build "/code/contracts/mycontract".
 # Note: if contractdir is "." (default in Docker), this ends up as a noop
-for contractdir in $contractdirs
-do
-echo "Building contract in $(realpath -m "$contractdir")"
-(
-  cd "$contractdir"
+for contractdir in $contractdirs; do
+  echo "Building contract in $(realpath -m "$contractdir")"
+  (
+    cd "$contractdir"
 
-  # Linker flag "-s" for stripping (https://github.com/rust-lang/cargo/issues/3483#issuecomment-431209957)
-  # Note that shortcuts from .cargo/config are not available in source code packages from crates.io
-  RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown --locked
-)
+    # Linker flag "-s" for stripping (https://github.com/rust-lang/cargo/issues/3483#issuecomment-431209957)
+    # Note that shortcuts from .cargo/config are not available in source code packages from crates.io
+    RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown --locked
+  )
 
-# wasm-optimize on all results
-for wasm in "$contractdir"/target/wasm32-unknown-unknown/release/*.wasm; do
-  name=$(basename "$wasm")
-  echo "Optimizing $name"
-  wasm-opt -Os "$wasm" -o "artifacts/$name"
+  # wasm-optimize on all results
+  for wasm in "$contractdir"/target/wasm32-unknown-unknown/release/*.wasm; do
+    name=$(basename "$wasm")
+    echo "Optimizing $name"
+    wasm-opt -Os "$wasm" -o "artifacts/$name"
+  done
 done
 
 # create hash
@@ -49,4 +49,3 @@ echo "Info: sccache stats after build"
 sccache -s
 
 echo "done"
-done
