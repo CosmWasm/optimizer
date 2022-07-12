a0184cc2214049bbcff7c7fb495c95d93f5f5be4
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2022-07-12 02:59:54 +0200

    Support incremental workspace builds

--- optimize_workspace.sh
+++ optimize_workspace.sh
@@ -18,29 +18,38 @@ cargo --version
 rm -f target/wasm32-unknown-unknown/release/*.wasm
 
 # Build artifacts
-echo "Building artifacts in workspace ..."
+echo "Building artifacts in workspace..."
 /usr/local/bin/build_workspace
 
 mkdir -p artifacts
-echo "Creating intermediate hashes ..."
-sha256sum -- target/wasm32-unknown-unknown/release/*.wasm | tee artifacts/checksums_intermediate.txt
-
-echo "Optimizing artifacts in workspace ..."
+echo "Optimizing artifacts in workspace..."
 TMPARTIFACTS=$(mktemp -p "$(pwd)" -d artifacts.XXXXXX)
 # Optimize artifacts
 (
   cd "$TMPARTIFACTS"
 
   for WASM in ../target/wasm32-unknown-unknown/release/*.wasm; do
-    NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
-    echo "Optimizing $NAME ..."
-    wasm-opt -Os "$WASM" -o "$NAME"
+    BASENAME=$(basename "$WASM" .wasm)
+    NAME=${BASENAME}${SUFFIX}
+    FILENAME=${NAME}.wasm
+
+    SHA256=$(sha256sum -- "$WASM" | sed 's/..\/target/target/g')
+    INTERMEDIATES="../artifacts/checksums_intermediate.txt"
+    if grep -Fxq "$SHA256" "$INTERMEDIATES"; then
+      echo "$BASENAME unchanged. Skipping optimization."
+    else
+      grep -vs "$BASENAME" "$INTERMEDIATES" >tmp_shas && mv -f tmp_shas "$INTERMEDIATES"
+      echo "Creating intermediate hash for ${BASENAME}..."
+      echo "$SHA256" | tee -a "$INTERMEDIATES" >/dev/null
+      echo "Optimizing ${BASENAME}..."
+      wasm-opt -Os "$WASM" -o "$FILENAME"
+      echo "Moving wasm files..."
+      mv ./*.wasm ../artifacts
+    fi
   done
-  echo "Moving wasm files ..."
-  mv ./*.wasm ../artifacts
 )
 rm -rf "$TMPARTIFACTS"
-echo "Post-processing artifacts in workspace ..."
+echo "Post-processing artifacts in workspace..."
 (
   cd artifacts
   sha256sum -- *.wasm | tee checksums.txt
