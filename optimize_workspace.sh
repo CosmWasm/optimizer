41aa6797dc9925f4410dc03ab9e70c18eb8e53fc
Simon Warta <simon@warta.it>
2023-10-23 18:03:45 +0200

    Remove SKIP_OPTIMIZATION

--- optimize_workspace.sh
+++ optimize_workspace.sh
@@ -40,7 +40,6 @@ TMPARTIFACTS=$(mktemp -p "$(pwd)" -d artifacts.XXXXXX)
 (
   cd "$TMPARTIFACTS"
   INTERMEDIATE_SHAS="../artifacts/checksums_intermediate.txt"
-  OPTIMIZED_SHAS="../artifacts/checksums.txt"
 
   for WASM in /target/wasm32-unknown-unknown/release/*.wasm; do
     BASENAME=$(basename "$WASM" .wasm)
@@ -49,39 +48,19 @@ TMPARTIFACTS=$(mktemp -p "$(pwd)" -d artifacts.XXXXXX)
 
     INTERMEDIATE_SHA=$(sha256sum -- "$WASM" | sed 's,/target,target,g')
 
-    SKIP_OPTIMIZATION=false
-    if test -f "../artifacts/${OPTIMIZED_WASM}"; then
-      INTERMEDIATE_CACHE_HIT=$(
-        grep -Fxsq "$INTERMEDIATE_SHA" "$INTERMEDIATE_SHAS"
-        echo $?
-      )
-      OPTIMIZED_SHA=$(sha256sum -- "../artifacts/$OPTIMIZED_WASM" | sed 's,../artifacts/,,g')
-      OPTIMIZED_CACHE_HIT=$(
-        grep -Fxsq "$OPTIMIZED_SHA" "$OPTIMIZED_SHAS"
-        echo $?
-      )
-      if [ "$INTERMEDIATE_CACHE_HIT" -eq 0 ] && [ "$OPTIMIZED_CACHE_HIT" -eq 0 ]; then
-        SKIP_OPTIMIZATION=true
-      fi
-    fi
-
-    if [ "$SKIP_OPTIMIZATION" = true ]; then
-      echo "$BASENAME unchanged. Skipping optimization."
+    if test -f "$INTERMEDIATE_SHAS"; then
+      echo "Updating intermediate hash for ${BASENAME}..."
+      sed -ni "/$BASENAME/!p" "$INTERMEDIATE_SHAS"
     else
-      if test -f "$INTERMEDIATE_SHAS"; then
-        echo "Updating intermediate hash for ${BASENAME}..."
-        sed -ni "/$BASENAME/!p" "$INTERMEDIATE_SHAS"
-      else
-        echo "Creating intermediate hash for ${BASENAME}..."
-      fi
-      echo "$INTERMEDIATE_SHA" >>"$INTERMEDIATE_SHAS"
-
-      echo "Optimizing ${BASENAME}..."
-      # --signext-lowering is needed to support blockchains runnning CosmWasm < 1.3. It can be removed eventually
-      wasm-opt -Os --signext-lowering "$WASM" -o "$OPTIMIZED_WASM"
-      echo "Moving ${OPTIMIZED_WASM}..."
-      mv "$OPTIMIZED_WASM" ../artifacts
+      echo "Creating intermediate hash for ${BASENAME}..."
     fi
+    echo "$INTERMEDIATE_SHA" >>"$INTERMEDIATE_SHAS"
+
+    echo "Optimizing ${BASENAME}..."
+    # --signext-lowering is needed to support blockchains runnning CosmWasm < 1.3. It can be removed eventually
+    wasm-opt -Os --signext-lowering "$WASM" -o "$OPTIMIZED_WASM"
+    echo "Moving ${OPTIMIZED_WASM}..."
+    mv "$OPTIMIZED_WASM" ../artifacts
   done
 )
 rm -rf "$TMPARTIFACTS"
