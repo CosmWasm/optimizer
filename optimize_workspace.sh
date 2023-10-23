6838510ba5944fd80c7eedfb5825baa41561edab
Simon Warta <simon@warta.it>
2023-10-23 18:26:11 +0200

    Add comment on path argument

--- optimize_workspace.sh
+++ optimize_workspace.sh
@@ -23,6 +23,16 @@ mkdir -p artifacts
 # old builds in cache should not be contained in the result of the next build.
 rm -f /target/wasm32-unknown-unknown/release/*.wasm
 
+# There are two cases here
+# 1. The contract is included in the root workspace (eg. `cosmwasm-template`)
+#    In this case, we pass no argument, just mount the proper directory.
+# 2. Contracts are excluded from the root workspace, but import relative paths from other packages (only `cosmwasm`).
+#    In this case, we mount root workspace and pass in a path `docker run <repo> ./contracts/hackatom`
+
+# This parameter allows us to mount a folder into docker container's "/code"
+# and build "/code/contracts/mycontract".
+# The default value for $1 is "." (see CMD in the Dockerfile).
+
 # Ensure we get exactly one argument and this is a directory (the path to the Cargo project to be built)
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   echo "Usage: $0 DIRECTORY" >&2
